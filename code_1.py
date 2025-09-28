c2db806f681896caf43581a0e32aa3f660c8638c
Bieltm <bieltorruella2006@gmail.com>
2025-09-28 20:38:20 +0200

    add dates into json

--- code_1.py
+++ code_1.py
@@ -4,21 +4,23 @@ with open("file.json", "r") as file:
     data = json.load(file)
 comand = ""
 while comand != "exit":
-    comand = input("")
+    comand = input("enter: (add/show/completed/exit): ").lower()
     if comand == "add":
         new_chore = input("Enter a new chore: ")
-        #saves the new chore to the list
-        data['chores'].append(new_chore)
+        new_date = input("Enter the due date (MM/DD): ")
+        #saves the new chore and data to the list
+        data['tasks'].append({"chore": new_chore, "date": new_date})
         #saves the changes to the json file
         with open("file.json", "w") as file:
             json.dump(data, file, indent=4)
     if comand == "show":
-        for chore in data['chores']:
-            print(chore)
+        #TODO: add priority levels
+        for task in data['tasks']:
+            print(f"Chore: {task['chore']}, Due Date: {task['date']}")  
     if comand == "completed":
         chore_done = input("Enter the chore you completed: ")
-        if chore_done in data['chores']:
-            data['chores'].remove(chore_done)
+        if chore_done in [task['chore'] for task in data['tasks']]:
+            data['tasks'].remove({"chore": chore_done})
             with open("file.json", "w") as file:
                 json.dump(data, file, indent=4)
             print(f"Chore '{chore_done}' marked as completed and removed from the list.")
