791a6b9c9349c6be40df4896955a9e5bbfcc53b2
Bieltm <bieltorruella2006@gmail.com>
2025-09-29 11:07:12 +0200

    add priority levels by colors and order, and added tags

--- code_1.py
+++ code_1.py
@@ -8,16 +8,33 @@ while comand != "exit":
     if comand == "add":
         new_chore = input("Enter a new chore: ")
         new_date = input("Enter the due date (MM/DD): ")
+        new_priority = input("Enter the priority (high/medium/low): ").lower()
+        while new_priority not in ["high", "medium", "low"]:
+            print("Invalid priority. Please enter 'high', 'medium', or 'low'.")
+            new_priority = input("Enter the priority (high/medium/low): ").lower()
+        new_tag = input("Enter a tag for the chore : ").lower()
+        while new_tag not in ["academic", "sport", "other"]:
+            print("Invalid tag. Please enter 'academic', 'sport', or 'other'.")
+            new_tag = input("Enter a tag for the chore : ").lower()
         #saves the new chore and data to the list
-        data['tasks'].append({"chore": new_chore, "date": new_date})
+        data['tasks'].append({"chore": new_chore, "date": new_date, "priority": new_priority, "tag": new_tag})
         #saves the changes to the json file
         with open("file.json", "w") as file:
             json.dump(data, file, indent=4)
     if comand == "show":
-        #TODO: add priority levels
-        for task in data['tasks']:
-            print(f"Chore: {task['chore']}, Due Date: {task['date']}")  
+        levels = ["high", "medium", "low"]
+        colors = {
+            "high": "\033[92m",    # Red
+            "medium": "\033[93m",  # Yellow
+            "low": "\033[91m"      # Green
+        }
+        for i in levels:
+            for task in data['tasks']:
+                if (task['priority'] == i):
+                    print(f"{colors[i]}Chore: {task['chore']}, Due Date: {task['date']}, Tag: {task['tag']}")
+        print("\033[0m")  # Reset color
     if comand == "completed":
+        #Fix Error
         chore_done = input("Enter the chore you completed: ")
         if chore_done in [task['chore'] for task in data['tasks']]:
             data['tasks'].remove({"chore": chore_done})
